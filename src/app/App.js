d3787f87d4b791222d88953f7ab21bf3bbd903fa
apache8080 <desai.rishi1@gmail.com>
2017-12-08 14:13:57 -0600

    fixed random loading gif issue

--- src/app/App.js
+++ src/app/App.js
@@ -13,49 +13,56 @@ class App extends Component {
 			  details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
 			  img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/115px-Tensorflow_logo.svg.png",
 			  image: "acmuiuc/tensorflow",
-			  disabled: false
+			  disabled: false,
+			  loading: false
 		      },
 		       {
 			   name:'Caffe2',
 			   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
 			   img: "https://caffe2.ai/static/logo.svg",
 			   image: "registry.gitlab.com/acm-uiuc/gpu-cluster-images:caffe2",
-			   disabled: true
+			   disabled: true,
+			   loading: false
 		       },
 		       {
 			   name:'PyTorch',
 			   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
 			   img: "http://pytorch.org/static/img/pytorch-logo-dark.svg",
 			   image: "acmuiuc/pytorch",
-			   disabled: false
+			   disabled: false,
+			   loading: false
 		       },
 		       {
 			   name:'Keras',
 			   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
 			   img: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg",
 			   image: "acmuiuc/keras",
-			   disabled: false
+			   disabled: false,
+			   loading: false
 		       },
 		       {
 			   name:'NVIDIA Digits',
 			   details: "Ubuntu 16.04 - In Browser GUI",
 			   img: "https://www.scan.co.uk/images/3xs/infopages/nvidia-digits.png",
 			   image: "nvidia/digits",
-			   disabled: true
+			   disabled: true,
+			   loading: false
 		       },
 		       {
 			   name:'Caffe',
 			   details: "Ubuntu 16.04 - C++ - SSH",
 			   img: "http://antmicro.com/images/logos/products-tk-caffe.svg",
 			   image: "nvidia/caffe",
-			   disabled: true
+			   disabled: true,
+			   loading: false
 		       },
 		       {
 			   name:'Dockerfile',
 			   details: null,
 			   img: "https://cdn.worldvectorlogo.com/logos/docker.svg",
 			   image: null,
-			   disabled: true
+			   disabled: true,
+			   loading: false
 		       }],
 		      disableAllButton: false
 		     };
@@ -65,8 +72,10 @@ class App extends Component {
         
     click(f) {
 	var updatedFramework = this.state.framework;
+	var type = typeof updatedFramework;
+	console.log(type);
 	var index = updatedFramework.indexOf(f);
-	updatedFramework[index].disabled = true;
+	updatedFramework[index].loading = true;
 	this.setState({framework:updatedFramework, disableAllButton:true})
 	
 	fetch('http://vault.acm.illinois.edu:5656/create_container', {
