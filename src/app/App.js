9b12d1a5765a078a93fa8d6281998df55e7a0982
Naren Dasan <naren@narendasan.com>
2017-12-08 11:10:16 -0600

    footer

--- src/app/App.js
+++ src/app/App.js
@@ -1,57 +1,50 @@
 import React, { Component } from 'react';
 import Frameworks from './frameworks/Frameworks.js'
 import Nav from './nav/Nav.js'
+import Footer from './footer/Footer.js'
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
 
-        this.state = {frameworks: 
-		      [{
-			  name:'TensorFlow',
-			  details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
-			  img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/115px-Tensorflow_logo.svg.png",
-			  image: "acmuiuc/tensorflow"
-		      },
-		       {
-			   name:'Caffe2',
-			   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
-			   img: "https://caffe2.ai/static/logo.svg",
-			   image: "registry.gitlab.com/acm-uiuc/gpu-cluster-images:caffe2"
-		       },
-		       {
-			   name:'PyTorch',
-			   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
-			   img: "http://pytorch.org/static/img/pytorch-logo-dark.svg",
-			   image: "acmuiuc/pytorch"
-		       },
-		       {
-			   name:'Keras',
-			   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
-			   img: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg",
-			   image: "acmuiuc/keras"
-		       },
-		       {
-			   name:'NVIDIA Digits',
-			   details: "Ubuntu 16.04 - In Browser GUI",
-			   img: "https://www.scan.co.uk/images/3xs/infopages/nvidia-digits.png",
-			   image: "nvidia/digits"
-		       },
-		       {
-			   name:'Caffe',
-			   details: "Ubuntu 16.04 - C++ - SSH",
-			   img: "http://antmicro.com/images/logos/products-tk-caffe.svg",
-			   image: "nvidia/caffe"
-		       },
-		       {
-			   name:'Dockerfile',
-			   details: null,
-			   img: "https://cdn.worldvectorlogo.com/logos/docker.svg",
-			   image: null
-		       }],
-		      disableButton: false
-		     };
+        this.state = {frameworks: [{
+										name:'TensorFlow',
+										details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
+										img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/115px-Tensorflow_logo.svg.png",
+										image: "acmuiuc/tensorflow"
+									},{
+										name:'Caffe2',
+										details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
+										img: "https://caffe2.ai/static/logo.svg",
+										image: "registry.gitlab.com/acm-uiuc/gpu-cluster-images:caffe2"
+									},{
+										name:'PyTorch',
+										details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
+										img: "http://pytorch.org/static/img/pytorch-logo-dark.svg",
+										image: "acmuiuc/pytorch"
+									},{
+										name:'Keras',
+										details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
+										img: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg",
+										image: "acmuiuc/keras"
+									},{
+										name:'NVIDIA Digits',
+										details: "Ubuntu 16.04 - In Browser GUI",
+										img: "https://www.scan.co.uk/images/3xs/infopages/nvidia-digits.png",
+										image: "nvidia/digits"
+									},{
+										name:'Caffe',
+										details: "Ubuntu 16.04 - C++ - SSH",
+										img: "http://antmicro.com/images/logos/products-tk-caffe.svg",
+										image: "nvidia/caffe"
+									},{
+										name:'Dockerfile',
+										details: null,
+										img: "https://cdn.worldvectorlogo.com/logos/docker.svg",
+										image: null
+									}],
+									disableButton: false};
 
         this.click = this.click.bind(this);
     }
@@ -75,10 +68,11 @@ class App extends Component {
     render() {
 	console.log(this.state.disableButton);
 	return (
-              <div className="GPU-CLUSTER-FRONTEND">
-                  <Nav/> 
+            <div className="GPU-CLUSTER-FRONTEND">
+                <Nav/> 
                 <Frameworks disabled={this.state.disableButton} frameworks={this.state.frameworks} handler={this.click} />
-              </div>
+				<Footer/>
+			</div>
         );
     }
 }
--- src/app/footer/footer.js
+++ src/app/footer/footer.js
@@ -1,33 +1,34 @@
 import React, {Component} from 'react';
-import './Nav.css'
+import './Footer.css'
+import FontAwesome from 'react-fontawesome' ;
 
 class Footer extends Component {
     render() {
         return ( 
-            <footer class="footer">
-                <div class="small-4 medium-4 large-4 columns">
+            <footer className="footer">
+                <div className="small-4 medium-4 large-4 columns">
                     <a id="illinois" href="https://illinois.edu" target="_blank">
-                        <img class="logo" id="illinois" src="/images/illinois_black.svg"></img>
+                        <img class="logo" id="illinois" src="/new_illinois.svg"></img>
                     </a>
                 </div>
-                <div class="small-4 medium-4 large-4 columns">
+                <div className="small-4 medium-4 large-4 columns">
                     <p>&copy; 2017 ACM@UIUC</p>
                 </div>
-                <div class="small-4 medium-4 large-4 columns">
-                    <a  href="https://github.com/acm-uiuc" target="_blank">
-                        <FontAwesome className="fa fa-github logo" id="github" aria-hidden="true"></i>
+                <div className="small-4 medium-4 large-4 columns logos">
+                    <a  href="https://github.com/acm-uiuc" className="logolink" target="_blank">
+                        <FontAwesome name='github' size="lg" className="github logo" id="github" aria-hidden="true"/>
                     </a>
-                    <a  href="https://twitter.com/acmuiuc" target="_blank">
-                        <FontAwesome className="fa fa-twitter logo" id="twitter" aria-hidden="true"></i>
+                    <a  href="https://twitter.com/acmuiuc" className="logolink" target="_blank">
+                        <FontAwesome name='twitter' size="lg" className="twitter logo" id="twitter" aria-hidden="true"/>
                     </a>
-                    <a  href="https://www.facebook.com/acmuiuc" target="_blank">
-                        <FontAwesome className="fa fa-facebook logo" id="fb" aria-hidden="true"></i>
+                    <a  href="https://www.facebook.com/acmuiuc" className="logolink" target="_blank">
+                        <FontAwesome name='facebook' size="lg" className="facebook logo" id="fb" aria-hidden="true"/>
                     </a>     
-                    <a  href="https://medium.com/@acmuiuc" target="_blank">
-                        <FontAwesome className="fa fa-medium logo" id="medium" aria-hidden="true"></i>
+                    <a  href="https://medium.com/@acmuiuc" className="logolink" target="_blank">
+                        <FontAwesome name='medium' size="lg" className="medium logo" id="medium" aria-hidden="true"/>
                     </a>  
                 </div>
-        </footer>
+            </footer>
         )
     }
 }
