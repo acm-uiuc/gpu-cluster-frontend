2e89d1652f6c8f41e57f4b2c654465985bc0fc2f
Naren Dasan <naren@narendasan.com>
2017-12-20 17:12:38 -0600

    add cntk and mxnet

--- src/frameworkImages.js
+++ src/frameworkImages.js
@@ -3,22 +3,22 @@ var FrameworkImages =
   name:'TensorFlow',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/115px-Tensorflow_logo.svg.png",
-  image: "acmuiuc/tensorflow"
+  image: "acmuiuc/tensorflow:latest"
 },{
   name:'Caffe2',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://caffe2.ai/static/logo.svg",
-  image: "registry.gitlab.com/acm-uiuc/gpu-cluster-images:caffe2"
+  image: "acmuiuc/caffe2:latest"
 },{
   name:'PyTorch',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "http://pytorch.org/static/img/pytorch-logo-dark.svg",
-  image: "acmuiuc/pytorch"
+  image: "acmuiuc/pytorch:latest"
 },{
   name:'Keras',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg",
-  image: "acmuiuc/keras"
+  image: "acmuiuc/keras:latest"
 },{
   name:'NVIDIA Digits',
   details: "Ubuntu 16.04 - In Browser GUI",
@@ -29,6 +29,16 @@ var FrameworkImages =
   details: "Ubuntu 16.04 - C++ - SSH",
   img: "http://antmicro.com/images/logos/products-tk-caffe.svg",
   image: "nvidia/caffe"
+},{
+  name:'mxnet',
+  details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
+  img: "https://secure.gravatar.com/avatar/04d69822f9bb98f4d8fd1484c7954c3d.jpg?s=512&r=g&d=mm",
+  image: "acmuiuc/mxnet"
+},{
+  name:'CNTK',
+  details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
+  img: "https://blog.paperspace.com/content/images/2016/12/cntk.png",
+  image: "acmuiuc/cntk"
 },{
   name:'Dockerfile',
   details: null,
