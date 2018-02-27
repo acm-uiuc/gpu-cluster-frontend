5b79bbf779095b7fb7f6d5f3e7de7ecc557e0477
Naren Dasan <naren@narendasan.com>
2018-02-27 10:45:49 -0600

    ready to test

--- src/app/App.js
+++ src/app/App.js
@@ -6,43 +6,45 @@ import './App.css';
 
 class App extends Component {
     constructor(props) {
-	super(props);
-        this.state = {frameworks: this.props.frameworkImages,
-                      disableAllButton: false};
-	this.loadingGif = "https://loading.io/spinners/double-ring/lg.double-ring-spinner.gif";
-	this.click = this.click.bind(this);
-	this.api = window.location.protocol+"//"+window.location.href.split('/')[2]
+        super(props);
+            this.state = {frameworks: this.props.frameworkImages,
+                          disableAllButtons: false,
+                          selects: Array.apply(null, Array(this.props.frameworkImages.length)).map(Number.prototype.valueOf,0),
+                          ui_url: null};
+        this.loadingGif = "https://loading.io/spinners/double-ring/lg.double-ring-spinner.gif";
+        this.click = this.click.bind(this);
+        this.api = window.location.protocol+"//"+window.location.href.split('/')[2];
     }
         
-    click(f) {
-        var updatedFramework = this.state.frameworks
-        var type = typeof updatedFramework;
-        console.log(type);
+    select(f) {
+        var updatedFramework = this.state.frameworks;
         var index = updatedFramework.indexOf(f);
         updatedFramework[index].loading = true;
-        this.setState({framework:updatedFramework, disableAllButton:true})
+        this.setState({framework:updatedFramework, disableAllButtons:true});
 
         fetch(this.api + '/create_container', {
-                  method: 'POST',
-                  headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(f)
-          }).then((resp) => resp.json()).then(function(res){
-                window.location = res['ui_url']; 
-	        });
-          console.log(f)
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(f)
+            }).then((resp) => resp.json()).then(function(res){
+                this.setState({ui_url: res['ui_url']}); 
+            });
+    }
+
+    confirm() {
+        window.location = this.state.ui_url;
     }
     
     render() {
-	      console.log(this.state.disableAllButton);
 	      return (
             <div className="GPU-CLUSTER-FRONTEND">
                 <Nav/> 
-                <Frameworks disabled={this.state.disableButton} frameworks={this.state.frameworks} handler={this.click} loadingGif={this.loadingGif} />
-				        <Footer/>
-			      </div>
+                <Frameworks disabled={this.state.disableAllButtons} frameworks={this.state.frameworks} selection={this.state.selects} confirm_handler={this.confirm} select_handler={this.select} loadingGif={this.loadingGif} />
+				<Footer/>
+			</div>
         );
     }
 }
--- src/app/frameworks/Frameworks.js
+++ src/app/frameworks/Frameworks.js
@@ -3,15 +3,15 @@ import './Frameworks.css'
 import Framework from './framework/Framework.js'
 
 class Grid extends Component {
-    renderFramework(f, t, cl) {
-        return <Framework disabled={this.props.disabled} framework={f} onClickHandler={this.props.handler} loadingGif={this.props.loadingGif}/>
+    renderFramework(f, i) {
+        return <Framework disabled={this.props.disabled} framework={f} onConfirmHandler={this.props.confirm_handler} onSelectHandler={this.props.select_handler} selected={this.props.selects[i]} loadingGif={this.props.loadingGif}/>
     }
 
     render() {
         var frameworks = []
         console.log(this.props.grid)
         for (var f = 0; f < this.props.frameworks.length; f++) {
-            frameworks.push(this.renderFramework(this.props.frameworks[f]));
+            frameworks.push(this.renderFramework(this.props.frameworks[f], f));
         }
 
         return (
