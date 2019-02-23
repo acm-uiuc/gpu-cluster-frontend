ac4cb5e79f029f128b156c80f4789038a51c3f18
Eric Zhang <ezhang887@gmail.com>
2019-02-23 16:21:34 -0600

    add Docker image input field

--- src/app/frameworks/Frameworks.js
+++ src/app/frameworks/Frameworks.js
@@ -3,17 +3,19 @@ import './Frameworks.css'
 import Framework from './framework/Framework.js'
 
 class Grid extends Component {
-    renderFramework(f, t, cl) {
-        return <Framework disabled={this.props.disabled} framework={f} onClickHandler={this.props.handler} loadingGif={this.props.loadingGif}/>
+    renderFramework(f, d) {
+        return <Framework disabled={this.props.disabled} framework={f} onClickHandler={this.props.handler} loadingGif={this.props.loadingGif} docker={d}/>
     }
 
     render() {
         var frameworks = []
-        console.log(this.props.grid)
         for (var f = 0; f < this.props.frameworks.length; f++) {
-            frameworks.push(this.renderFramework(this.props.frameworks[f]));
+            var docker = false;
+            if (this.props.frameworks[f].name==='Dockerfile'){
+                docker = true;
+            }
+            frameworks.push(this.renderFramework(this.props.frameworks[f], docker));
         }
-
         return (
             <div className="Frameworks">
                 {frameworks}
--- src/app/frameworks/framework/Framework.js
+++ src/app/frameworks/framework/Framework.js
@@ -2,8 +2,31 @@ import React, {Component} from 'react';
 import './Framework.css'
 
 class Framework extends Component {
+        
     render() {
-	console.log(this.props.disabled);
+        if (this.props.docker){
+            var value = document.getElementById("docker_img");
+            if (value != null && value.value !== ""){
+                console.log(value.value);
+                this.props.framework.image = value.value;
+                console.log(this.props.framework);
+            }
+            else{
+                this.props.framework.image = null;
+            }
+            return (
+                <button className="Framework" onClick={() => this.props.onClickHandler(this.props.framework)} disabled={this.props.disabled || this.props.framework.disabled}>
+                <img className="logo" src={this.props.framework.loading && value.value !=""? this.props.loadingGif : this.props.framework.img} alt=""/>
+                <div>
+                    <input type="text" name = "docker_img" id = "docker_img" />
+                </div>
+                <div className="label"> 
+                    <span className="name">{this.props.framework.name}</span>
+                    <span className="details">{this.props.framework.details}</span>
+                </div>
+            </button>
+            )
+        }
         return (
 		<button className="Framework" onClick={() => this.props.onClickHandler(this.props.framework)} disabled={this.props.disabled || this.props.framework.disabled}>
                 <img className="logo" src={this.props.framework.loading ? this.props.loadingGif : this.props.framework.img} alt=""/>
