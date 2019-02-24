7e91c0a61ada741cbfe89d70688ec5be65288abd
Eric Zhang <ezhang887@gmail.com>
2019-02-24 03:20:14 -0600

    update POST body to tell if we need to pull a docker image from dockerhub

--- src/frameworkImages.js
+++ src/frameworkImages.js
@@ -4,52 +4,61 @@ var FrameworkImages =
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/115px-Tensorflow_logo.svg.png",
   image: "acmuiuc/tensorflow:latest",
-  token_required: true
+  token_required: true,
+  pull_image: false
 },{
   name:'Caffe2',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://caffe2.ai/static/logo.svg",
   image: "acmuiuc/caffe2:latest",
-  token_required: true
+  token_required: true,
+  pull_image: false
 },{
   name:'PyTorch',
   details: "Ubuntu 16.04 - Python 3.6 - Jupyter Notebook",
   img: "http://pytorch.org/static/img/pytorch-logo-dark.svg",
   image: "acmuiuc/pytorch:v0.2",
-  token_required: true
+  token_required: true,
+  pull_image: false
 },{
   name:'Keras',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Keras_Logo.jpg",
   image: "acmuiuc/keras:latest",
-  token_required: true 
+  token_required: true,
+  pull_image: false
 },{
   name:'NVIDIA Digits',
   details: "Ubuntu 16.04 - In Browser GUI",
   img: "https://www.scan.co.uk/images/3xs/infopages/nvidia-digits.png",
-  image: "acmuiuc/digits:latest"
+  image: "acmuiuc/digits:latest",
+  pull_image: false
 },{
   name:'Caffe',
   details: "Ubuntu 16.04 - C++ - SSH",
   img: "http://antmicro.com/images/logos/products-tk-caffe.svg",
-  image: "nvidia/caffe"
+  image: "nvidia/caffe",
+  pull_image: false
 },{
   name:'mxnet',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://pbs.twimg.com/profile_images/921441672684171264/ElXb2HlG_400x400.jpg",
   image: "acmuiuc/mxnet",
-  token_required: true
+  token_required: true,
+  pull_image: false
 },{
   name:'CNTK',
   details: "Ubuntu 16.04 - Python 3.5 - Jupyter Notebook",
   img: "https://blog.paperspace.com/content/images/2016/12/cntk.png",
   image: "acmuiuc/cntk",
-  token_required: true
+  token_required: true,
+  pull_image: false
 },{
   name:'Dockerfile',
   details: null,
   img: "https://cdn.worldvectorlogo.com/logos/docker.svg",
-  image: null
+  image: null,
+  pull_image: true
 }]
 
 module.exports = FrameworkImages;
